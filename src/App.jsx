c143e4fba86b958e26ba7af97b8ad6a178472ee3
victorleustean <victor.leustean@calistrathogas.ro>
2025-10-24 15:00:10 +0300

    Some more things

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./Components/Navbar/Navbar";
-
+import Hero from "./Components/Navbar/Hero"
 const App = () => {
 
   const current_theme = localStorage.getItem('current_theme');
@@ -12,6 +12,30 @@ const App = () => {
   return (
     <div className={`container ${theme}`}>
         <Navbar theme={theme} setTheme={setTheme}/>
+        <Hero/>
+        import CardSwap, { Card } from './CardSwap'
+
+<div style={{ height: '600px', position: 'relative' }}>
+  <CardSwap
+    cardDistance={60}
+    verticalDistance={70}
+    delay={5000}
+    pauseOnHover={false}
+  >
+    <Card>
+      <h3>Card 1</h3>
+      <p>Your content here</p>
+    </Card>
+    <Card>
+      <h3>Card 2</h3>
+      <p>Your content here</p>
+    </Card>
+    <Card>
+      <h3>Card 3</h3>
+      <p>Your content here</p>
+    </Card>
+  </CardSwap>
+</div>
     </div>
   )
 }
